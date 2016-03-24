853922b13f950479124d90205c5022b76a82dc32
Bilgin Sherifov <ersoybilgin@gmail.com>
2016-03-24 17:16:48 +0000

    Dump file

---
+++ del.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+
+switches = {'pandas': False, 'numpy': False, 'html': False,
+            'numba': False, 'replace': False, 'numexpr': False,
+            'ctypes': False, 'cython': True}
+
+print('\n', switches, '\n')
